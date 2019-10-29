a5318da1f58765ae34c4f09a9944e951e9b4bfb2
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2019-10-29 09:56:17 -0400

    Tirei rota para localhost

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-//const api = axios.create({ baseURL: 'https://socialme-api.herokuapp.com/ergCNTis' });
 const api = axios.create({ 
     baseURL: 'https://social-me-v2.herokuapp.com/ergCNTis',
-    //baseURL: 'http://192.168.2.28:3000/ergCNTis',
     headers: {'mundo-data-token': localStorage.getItem('token')}
 });
 
