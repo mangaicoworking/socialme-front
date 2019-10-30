3179205fbf4da729cb65496383c7085500f0cacc
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2019-10-30 16:49:31 -0400

    api ondelaine

--- src/Services/api.js
+++ src/Services/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 let token = localStorage.getItem('token');
 //let refreshToken = () => {return localStorage.getItem('token');}
 const api = axios.create({ 
-    //baseURL: 'https://social-me-v2.herokuapp.com/ergCNTis',
-    baseURL: 'http://192.168.2.28:3000/ergCNTis',
+    baseURL: 'https://social-me-v2.herokuapp.com/ergCNTis',
+    //baseURL: 'http://192.168.2.28:3000/ergCNTis',
     headers: {'mundo-data-token': token}
 });
 
